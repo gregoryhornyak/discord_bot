444c6e6c4bca0bf47c5accc1294a34bc6daacc27
gergely <hornyakj@coventry.ac.uk>
2023-02-25 13:26:41 +0000

    basic_bot py

---
+++ basic_bot.py
@@ -0,0 +1,107 @@
+# This example requires the 'members' and 'message_content' privileged intents to function.
+
+import discord
+from discord.ext import commands
+import random
+
+description = '''An example bot to showcase the discord.ext.commands extension
+module.
+
+There are a number of utility commands being showcased here.'''
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
+
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+@bot.command(description='For when you wanna settle the score some other way')
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def dako(ctx, length):
+    mid = "="
+    mid += "="*int(length)
+    await ctx.send(f"8{mid}D")
+
+@bot.command()
+async def whoami(ctx):
+    for i in range(1):
+        await ctx.send(f"You are {ctx.author.name}")
+
+@bot.command()
+async def what(ctx):
+    await ctx.send("""To guess a score: SIGN+guess -> SCORE
+To see every guess: SIGN+board
+To only see your guesses: SIGN+myboard""")
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def lajos(ctx):
+    script = """-Szia Lajos.
+-Szia bazdmeg! Kutyáidat sétáltatod?
+-Hát bazdmeg
+-Ilyen...ilyen szerelésbe?
+-Hát miér milyenbe?
+-Miért nem öltözöl föl rendesen?
+-Hát miér hát nem vagyok rendesen bazdmeg?
+-Na jólvan.
+-Most vettem fel bazdmeg délután!
+-Ilyen... hát kár volt bazdmeg
+-Hát ja
+-Na jólvan szia
+-Szia"""
+    for line in script.split('\n'):
+        await ctx.send(line)
+
+@bot.group()
+async def cool(ctx):
+    """Says if a user is cool.
+
+    In reality this just checks if a subcommand is being invoked.
+    """
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+
+TOKEN = 'MTA3ODQyNTM4ODE4ODk3NTEwNA.GGFXVa.liDofuZhcmrVSDWCHhQz01C4vvwB34ElErwg-k'
+
+bot.run(TOKEN)
