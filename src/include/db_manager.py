d61f93357db693918b98b9e0e0918ebdc00ee9e4
gregoryhornyak <gergely2125@gmail.com>
2023-04-24 19:22:32 +0200

    global location

--- src/include/db_manager.py
+++ src/include/db_manager.py
@@ -1,17 +1,9 @@
-"""
-IF append:
-- check if db exists
-    - if not then create one
-- otherwise append
-
-IF read:
-
-"""
-
 import json
 
+DB_LOCATION = 'src/databases/'
+
 def check_db_exists(db_name):
-    db_name = "src/databases/"+db_name
+    db_name = DB_LOCATION+db_name
     db = {}
     try:
         with open(db_name,'r') as f:
@@ -25,20 +17,21 @@ def check_db_exists(db_name):
             return True
         return False
 
-def write_db(db_name):
+def write_db(db_name): # fix json saving
     db = {'TIMESTAMP':{'event':'','user':'','guess':''}}
-    db_name = "../src/databases/"+db_name
+    db_name = DB_LOCATION+db_name
     with open(db_name,'w') as f:
         json.dump(db,f)
     pass
 
 def read_db(db_name):
-    db_name = "../src/databases/"+db_name
+    db_name = DB_LOCATION+db_name
     with open(db_name,'r') as f:
         db = json.load(f)
     return db
 
 def append_db(db_name,present,user,event,guess):
+    print(f"appending started to {db_name} with {event} and {guess}")
     if not check_db_exists(db_name):
         write_db(db_name)
     db = read_db(db_name)
@@ -46,18 +39,19 @@ def append_db(db_name,present,user,event,guess):
     db[present]['user'] = user
     db[present]['event'] = event
     db[present]['guess'] = guess
-    db_name = "../src/databases/"+db_name 
+    db_name = DB_LOCATION+db_name 
     # this db_name directory path should be resolved, as rn it has to be inserted to each function
     # resolve directory path issue: for both databases and the modules
-    with open(db_name,'w') as f:
+    with open(db_name,'w') as f: # should be 'a' as append
         json.dump(db,f)
+    print(f"DB-MANAGER appended guess to {db_name}")
     """
     for k,v in db.items():
         print(k,v)
     """
     return 0
 
-def last_entry(db_name,user,present):
+def last_entry(db_name,user,present):  # why 'present'?
     db = read_db(db_name)
     latest = '2023-01-01'
     for k,v in db.items():
