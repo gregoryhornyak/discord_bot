0e79596e303c8c60a00c67b28e4d6b8b9b9d9572
gregoryhornyak <gergely2125@gmail.com>
2023-09-24 01:40:34 +0200

    bot messages stored

--- include/constants.py
+++ include/constants.py
@@ -2,20 +2,21 @@ import json
 
 UPCOMING_DATE_PATH = "resources/data/"
 RESULTS_PATH = "resources/data/"
+FETCH_LOG_PATH = "resources/data/"
 TOKEN_PATH = "resources/token/"
 PASSW_PATH = "resources/password/"
 UPLOADS_PATH = "resources/uploads/"
-GUESS_FILE = "resources/data/"
-SCORES_FILE = "resources/scores/"
-USER_SCORES = "resources/data/"
-INVENTORY_PATH = "resources/inventory/"
+GUESS_DB_PATH = "resources/inventory/"
+SCORE_TABLE_PATH = "resources/inventory/"
+USERS_DB_PATH = "resources/inventory/"
 LOGS_PATH = "resources/logs/"
 MANIFEST_PATH = "docs/manifest/"
-SCORE_TABLE_PATH = "resources/inventory/"
-
-with open(f"{INVENTORY_PATH}score_table.json") as f:
-    score_table = json.load(f)
-
-F1_RACE_TYPES = [key for key in score_table.keys()]
 
 CHANNEL_ID = 1078427611597906004
+
+BOT_SHUTDOWN_UPGRD_MESSAGE = "Bot is shutting down and will be upgraded..."
+BOT_START_DETAILS_MESSAGE = f"bot_name\n\
+vversion [latest:latest_ver]\n\
+is now running...\n\
+Latest update: latest_update"
+BOT_START_SHORT_MESSAGE = "Bot is running..."
