405caa3d86d7d544eed8b52dab8493b5924c05d8
gregoryhornyak <gergely2125@gmail.com>
2023-09-29 00:45:54 +0200

    black listed words, better logs

--- include/logging_manager.py
+++ include/logging_manager.py
@@ -3,13 +3,13 @@ from include.constants import *
 
 logging.basicConfig(
     level=logging.DEBUG,
-    format='[%(asctime)s]: (%(levelname)s) %(message)s', datefmt="%m-%d %H:%M")
+    format='[%(asctime)s.%(msecs)03d]: %(name)s \t %(message)s', datefmt="%M:%S")
 
 # https://docs.python.org/3/library/logging.html#logging.Formatter
 
-file_handler = logging.FileHandler(f"{LOGS_PATH}botlogs.log")
+file_handler = logging.FileHandler(f"{LOGS_PATH}")
 file_handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('[%(asctime)s] - %(levelname)s { %(name)s: %(message)s')
+formatter = logging.Formatter('[%(asctime)s] %(levelname)s \t %(name)s \t %(message)s')
 file_handler.setFormatter(formatter)
 
 logging.getLogger().addHandler(file_handler)
