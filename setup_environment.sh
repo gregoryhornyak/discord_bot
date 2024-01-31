96314dac6708441f4c13c57f1adb8d4502d636bc
gregoryhornyak <hornyakj@coventry.ac.uk>
2024-01-31 17:40:38 +0000

    create resources dir tree

---
+++ setup_environment.sh
@@ -0,0 +1,20 @@
+echo "Creating resources directory"
+mkdir resources
+cd resources
+echo "Creating logs directory"
+mkdir logs
+echo "Creating token directory"
+mkdir token
+echo "Creating data directory"
+mkdir data
+echo "Creating inventory directory"
+mkdir inventory
+echo "Creating config directory"
+mkdir config
+echo "Creating uploads directory"
+mkdir uploads
+echo "Creating password directory"
+mkdir password
+cd ..
+tree
+echo "Finished setting up directories"
